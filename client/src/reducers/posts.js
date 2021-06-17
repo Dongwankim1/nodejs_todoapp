c191351cbd9b876bc341c69c18c4baaa7226f01f
dongwan <zkdlwu94@naver.com>
2021-06-17 18:57:30 +0900

    asd

---
+++ client/src/reducers/posts.js
@@ -0,0 +1,13 @@
+export default (state=[],action)=>{
+    if(action.type === 'CREATE'){
+        switch (action.type) {
+            case 'FETCH_ALL':
+                return state;
+            case 'CRETATE':
+                return state;
+        
+            default:
+                return state;
+        }
+    }
+}
